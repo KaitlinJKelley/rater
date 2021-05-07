a7fbf9a889abc08bb99c713efb2943113232d059
kjk1325e <kaitlinjkelley@gmail.comm>
2021-05-07 11:03:56 -0500

    Minor refactor to models; created fixtures to seed database

--- raterapp/models/game.py
+++ raterapp/models/game.py
@@ -4,7 +4,7 @@ class Game(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     designer = models.CharField(max_length=100)
-    release_year = models.IntegerField
+    release_year = models.IntegerField(default=0)
     num_of_players = models.CharField(max_length=20)
-    time_to_play = models. IntegerField
-    ages = models.CharField(max_length=20)
+    time_to_play = models. IntegerField(default=0)
+    min_age = models.IntegerField(default=21)
