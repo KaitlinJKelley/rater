fec73f2080606bdf65e78dae60eb32c5ca86d26e
kjk1325e <kaitlinjkelley@gmail.comm>
2021-05-12 12:58:55 -0500

    Added create function to GameViewSet

--- raterapp/views/game.py
+++ raterapp/views/game.py
@@ -5,9 +5,30 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from raterapp.models import User, Game, Category
+from raterapp.models import User, Game, Category, CategoryGame
 
 class GameViewSet(ViewSet):
+    def create(self, request):
+        game = Game()
+
+        game.title = request.data["title"]
+        game.description = request.data["description"]
+        game.designer = request.data["designer"]
+        game.release_year = request.data["releaseYear"]
+        game.num_of_players = request.data["numOfPlayers"]
+        game.time_to_play = request.data["timeToPlay"]
+        game.min_age = request.data["minAge"]
+
+        categories = Category.in_bulk(request.data["categories"])
+        CategoryGame.set(categories)
+
+        try:
+            game.save()
+            serializer = GameSerializer(game, context={"request": request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
     def retrieve(self, request, pk):
 
         try:
